6301061055f3286e780a66b7b1c6ab0a251983d9
alizamanian79 <alizamaniandev@gmail.com>
2024-09-07 09:56:03 +0330

    last session

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+from django.contrib import admin  
+from .models import Post  
 
-# Register your models here.
+# Register your models here.  
+class PostAdmin(admin.ModelAdmin):  
+    list_filter = ["created_time"]  
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Post (models.Model):
+    title=models.CharField(max_length=255)
+    description=models.TextField()
+    Active=models.BooleanField(default=False)
+    created_time=models.DateTimeField(auto_now_add=True)
+    updated_time=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name="Post"
+        verbose_name_plural = "Post's"
