8bbcee75e2968e745574f91b1e45c8bf708ab802
alizamanian79 <alizamaniandev@gmail.com>
2024-09-12 14:10:19 +0330

    rmv

--- website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import Index,About,Contact
-
-app_name='website'
-urlpatterns = [
-    path("",Index,name="index"),
-    path("about/",About,name="about"),
-    path("contact/",Contact,name="contact"),
-]
