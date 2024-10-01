3e7d2672998d561fbbd266096936d055203bf9f3
alizamanian79 <alizamaniandev@gmail.com>
2024-10-01 22:58:27 +0330

    login created

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from .views import login_view
-app_name='accounts'
-urlpatterns = [
-    path('login/',login_view,name='login'),
+from django.urls import path  
+from .views import login_view  
+
+app_name = 'accounts'  
+
+urlpatterns = [  
+    path('login/', login_view, {'mode': 'sign-in'}, name='login'),
+    path('login/<str:mode>/', login_view, name='mode'), 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
+from django.shortcuts import redirect
 
 # Create your views here.
-def login_view(request):
+def login_view(request, mode):  
+    if mode == "sign-in":  
+        if request.method == "POST":  
+            username = request.POST.get("username")  
+            password = request.POST.get("password")  
+            result = authenticate(request, username=username, password=password)  
+            if result is not None:  
+                login(request, result)
+                messages.success(request, "You are logged in successfully")  
+                return redirect("/")   
+            else:  
+                messages.error(request, "Wrong username or password")  
 
-    context={"form"}
-    return render(request,'accounts/login.html', context)
+    context = {"form": AuthenticationForm()} 
+    return render(request, 'accounts/login.html', context)  
