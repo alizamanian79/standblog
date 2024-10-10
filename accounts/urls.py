26ee5b02e4c70ab1e54fc53082d63bd7099ce779
alizamanian79 <alizamaniandev@gmail.com>
2024-10-10 10:00:56 +0330

    i dont know

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import login_view,logout_view,signup_view,CustomLogin
 
 app_name = 'accounts'  
-
 urlpatterns = [  
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
